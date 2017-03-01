0062ea9e52a0723063d30c0f2759ca8e7d66a8eb
newapple <734719526@qq.com>
2017-03-01 17:06:15 +0800

    GSON IMAGELOADER 初始化

--- app/src/main/java/com/example/administrator/shian_anzanggong/MainActivity.java
+++ app/src/main/java/com/example/administrator/shian_anzanggong/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.administrator.shian_anzanggong;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.administrator.shian_anzanggong.base.BaseActivity;
+import com.example.administrator.shian_anzanggong.base.BaseAppliction;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String url = "http://192.168.0.72/shianapp/home/index/index";
+        OkHttpUtils
+                .get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.v("this","response:"+response+" id:"+id);
+                        Toast.makeText(MainActivity.this, "response:"+response, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
