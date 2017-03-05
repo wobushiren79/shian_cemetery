b47c2493ee0ebf8a781b026814d386d23291febc
newapple <734719526@qq.com>
2017-03-05 11:49:39 +0800

    pull 初始化

--- app/src/main/java/com/example/administrator/shian_anzanggong/activity/SplashActivity.java
+++ app/src/main/java/com/example/administrator/shian_anzanggong/activity/SplashActivity.java
@@ -12,5 +12,7 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
     }
 }
