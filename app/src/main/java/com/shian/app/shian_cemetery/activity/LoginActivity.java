13e85e20b75bd8dfa7a78a4979d28f9ddd933b6d
newapple <734719526@qq.com>
2017-03-30 18:28:06 +0800

    包路径修改

---
+++ app/src/main/java/com/shian/app/shian_cemetery/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.shian.app.shian_cemetery.activity;
+
+import android.os.Bundle;
+
+import com.shian.app.shian_cemetery.R;
+import com.shian.app.shian_cemetery.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/shian/app/shian_cemetery/activity/SplashActivity.java
+++ app/src/main/java/com/shian/app/shian_cemetery/activity/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.shian.app.shian_cemetery.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.shian.app.shian_cemetery.base.BaseActivity;
@@ -7,11 +8,34 @@ import com.shian.app.shian_cemetery.R;
 
 public class SplashActivity extends BaseActivity {
 
+    private int SLEEPTIME = 3000;//屏幕休眠时间
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        sleepActivity();
+    }
 
+    /**
+     * 休眠3秒
+     */
+    private void sleepActivity() {
+        try {
+            Thread.sleep(SLEEPTIME);
+            jumpLogin();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 跳转登陆界面
+     */
+    private void jumpLogin() {
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
